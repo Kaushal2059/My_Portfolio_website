80b44f8b48107b9a479158c7e45a519f83340739
Kaushal20 <rupakhetikaushal7@gmail.com>
2026-06-11 09:06:19 +0545

    made UI changes for skills page

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
--- portfolio/portfolio_pages/models.py
+++ portfolio/portfolio_pages/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class About_me(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length= 240)
+    content = models.TextField()
     image = models.ImageField(upload_to='photos/', blank = True, null= True)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
