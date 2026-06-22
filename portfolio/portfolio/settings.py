b9878cb6d28230adee9618ab6b36bac4b5cc2b0e
Kaushal20 <rupakhetikaushal7@gmail.com>
2026-06-22 08:25:58 +0545

    added smoketest for CI pipeline

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -74,17 +74,24 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='portfolio_db'),
-        'USER': config('DB_USER', default='portfolio_user'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='db'),
-        'PORT': config('DB_PORT', default='5432'),
+if os.environ.get('TEST_DB') == 'sqlite':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME', default='portfolio_db'),
+            'USER': config('DB_USER', default='portfolio_user'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST', default='db'),
+            'PORT': config('DB_PORT', default='5432'),
+        }
+    }
 
 
 # Password validation
--- portfolio/portfolio_pages/tests.py
+++ portfolio/portfolio_pages/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class Smoketest(TestCase):
+    
+    def setUP(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username ='testuser',
+            password='testpass2'
+        )
+            
+    def test_homepage_loads(self):
+        response = self.client.get('/')
+        self.assertIn(response.status_code, [200, 301, 302])
+
+    def test_admin_page_loads(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_database_works(self):
+        count = User.objects.count()
+        self.assertGreaterEqual(count, 1)
+    
+    def test_authenticated_admin(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get('/admin/')
+        self.assertIn(response.status_code, [200, 302])
